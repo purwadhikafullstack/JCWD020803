516eff7f7a423cb2c53252d5afac5dd164046782
dimassagngsptr <dimasageng58@gmail.com>
2024-02-08 08:58:54 +0700

    fix: change file

--- apps/web/src/pages/user-dashboard/detail-vouchers/index.jsx
+++ apps/web/src/pages/user-dashboard/detail-vouchers/index.jsx
@@ -1,4 +1,4 @@
-import { UserVouchers } from '../../../components/customers/user-profile/profile-detail/voucher';
+import { UserVouchers } from '../../../components/customers/user-profile/profile-detail/voucher/index';
 
 function DetailVouchersPage() {
   return <UserVouchers />;
